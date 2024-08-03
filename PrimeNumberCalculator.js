8dcabd1ded092d32f088bcf2826ed0300d5194a0
Sebi <sebiki@web.de>
2024-08-03 15:08:42 +0200

    Moin, das ist mein Weg bis jetzt. Bin ansatzweise in der richtigen Richtung haha?

--- PrimeNumberCalculator.js
+++ PrimeNumberCalculator.js
@@ -1,3 +1,26 @@
  //Das ist mein erstes Arbeiten mit GitHub!
- console.log("Hallo Welt");
- 
+ console.log("Mein erstes Mal arbeiten mit Github");
+let count = 0;
+for (let i = 0; i < 100; i++){
+    if (isPrimeNumber(i)){
+        count++;
+    }
+};
+
+function isPrimeNumber(number){};
+
+function countPrimeNumber(limit){
+    let number1 = 1;
+    let number2 = 2;
+    let array = [];
+    let result = number1 +number2;
+    while ((result = (number1 + number2) % number2) <= limit){
+        if (result !== 0){
+            return result;
+        };
+        if (result === 0){
+            return faslse;
+        };
+
+    };
+};
