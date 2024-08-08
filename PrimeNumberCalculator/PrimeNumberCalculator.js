469369ce858f2f057006c4a36fd634d217e00e48
Sebi <sebiki@web.de>
2024-08-08 14:37:02 +0200

    Start der Challenges

--- PrimeNumberCalculator/PrimeNumberCalculator.js
+++ PrimeNumberCalculator/PrimeNumberCalculator.js
@@ -1,10 +1,40 @@
 console.log("Mein erstes Mal arbeiten mit Github");
-let number1 = 0;
-let number2 = 2;
-let result = number1 % number2; 
-for (let i = 0; i = 100; i++){
-    
+let count = 0;
+let primeNumbers = [];
+for (let i = 0; i < 100; i++){
+    if (isPrimeNumber(i)){
+        count++;
+        primeNumbers.push(i);
+    }
 };
-function countPrimeNumber(){};
 
-//0,1,3,5,7,11,13,15
+//Überprüft, ob die Zahl kleiner oder gleich 1 ist (solche Zahlen sind keine Primzahlen).
+//Prüft dann, ob die Zahl durch irgendeine Zahl von 2 bis zur Quadratwurzel der Zahl teilbar ist.
+//Wenn sie teilbar ist, ist sie keine Primzahl.
+//Wenn sie durch keine dieser Zahlen teilbar ist, ist sie eine Primzahl.
+function isPrimeNumber(number){
+    if (number <= 1){
+        return false;
+    };
+    for (let i = 2; i <= Math.sqrt(number); i++){
+        if (number % i === 0) {
+            return false;
+        };
+    };
+    return true;
+};
+
+//Die Schleife läuft von 0 bis 100.
+//Für jede Zahl wird die Funktion isPrimeNumber aufgerufen.
+//Wenn die Zahl eine Primzahl ist, wird sie mit console.log ausgegeben.
+for (let i = 0; i <= 100; i++){
+    if (isPrimeNumber(i)){
+        console.log(primeNumbers);
+    };
+};
+
+//Noch mit do while Schleife:
+// do {
+//     isPrimeNumber;
+// }
+// while (i <= 100);
