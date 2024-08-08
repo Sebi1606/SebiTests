7ce99de9a44388a7ede241d3352f6ad1d6559ed1
Sebi <sebiki@web.de>
2024-08-08 14:36:05 +0200

    Start mit Challenges

---
+++ PrimeNumberCalculator/PrimeNumberCalculator.js
@@ -0,0 +1,10 @@
+console.log("Mein erstes Mal arbeiten mit Github");
+let number1 = 0;
+let number2 = 2;
+let result = number1 % number2; 
+for (let i = 0; i = 100; i++){
+    
+};
+function countPrimeNumber(){};
+
+//0,1,3,5,7,11,13,15
