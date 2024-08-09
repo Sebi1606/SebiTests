06bde11389b108687d08424f2e894cebd5f5008c
Sebi <sebiki@web.de>
2024-08-09 14:50:40 +0200

    Funktionen 1 und 2 gelöst

---
+++ Notion/Funktionen1.js
@@ -0,0 +1,8 @@
+let a = parseFloat(prompt("Definiere Zahl A"));
+let b = parseFloat(prompt("Definiere Zahl B"));
+function add(a,b){
+    let result = a + b;
+    console.log(result);
+};
+
+add(a,b);
---
+++ Notion/Funktionen2.js
@@ -0,0 +1,8 @@
+let a = parseFloat(prompt("Definiere Zahl A"));
+let b = parseFloat(prompt("Definiere Zahl B"));
+function max(a,b){
+    let result = Math.max(a,b);
+    console.log(result);
+};
+
+max(a,b);
