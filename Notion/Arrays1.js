be9330082f1ba8816d8f9a2c251117f0a6502f44
Sebi <sebiki@web.de>
2024-08-23 12:46:11 +0200

    Array1

--- Notion/Arrays1.js
+++ Notion/Arrays1.js
@@ -1,1 +1,16 @@
-//sum(array) Funktion schreiben
+let array = [];
+
+let anzahlElemente = parseFloat(prompt("Gib die Anzahl der Elemente für das Array an"));
+for (let i = 0; i < anzahlElemente; i++){
+    let wert = parseFloat(prompt("Gib ein Wert für ein Element an"));
+    array[i] = wert;
+};
+
+console.log(array);
+
+sum(array);
+
+function sum(array){
+    let summe = array.reduce((accumulator, currenValue) => accumulator + currenValue, 0);
+    console.log(summe);
+};
