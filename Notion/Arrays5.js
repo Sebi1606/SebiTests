47bff91da7a6315143668e785b148d12bbf2f866
Sebi1606 <sebiki@web.de>
2024-08-28 14:56:49 +0200

    Verbessern und mit HTML Oberfläche machen

--- Notion/Arrays5.js
+++ Notion/Arrays5.js
@@ -3,26 +3,8 @@
 
 //Das ganze als Opeator, dass man den ganzen Code nicht sooo oft schreiben muss:
 
-let vector1 = [];
-let numberOfElements1 = parseFloat(prompt("Gib die Menge der Elemente für den ersten Vektor an"));
-for (let i = 0; i < numberOfElements1; i++){
-    let valueVector1 = parseFloat(prompt("Gib die Werte der Elemente für den ersten Vektor an"));
-    vector1[i] = valueVector1;
-};
-console.log(vector1);
-
-let vector2 = [];
-let numberOfElements2 = parseFloat(prompt("Gib die Menge der Elemente für den zweiten Vektor an"));
-for (let i = 0; i < numberOfElements2; i++){
-    let valueVector2 = parseFloat(prompt("Gib die Werte der Elemente für den zweiten Vektor an"));
-    vector2[i] = valueVector2;
-};
-
-console.log(vector2);
-
-let result = [];
-let operator = prompt("Gib an, welcher mathematische Operator (+, -, *, /) genutzt werden soll")
-function vectorTask(vector1,vector2, operator, result){
+function vectorTask(vector1,vector2, operator){
+    let result = [];
     if (operator === "+") {
         for (let i = 0; i < vector1.length; i++){
             result[i] = vector1[i] + vector2[i];
@@ -45,122 +27,186 @@ function vectorTask(vector1,vector2, operator, result){
     console.log(result);
 };
 
-vectorTask(vector1,vector2, operator, result);
 
-
-//Addieren:
-
-let vector1 = [];
-let numberOfElements1 = parseFloat(prompt("Gib die Menge der Elemente für den ersten Vektor an"));
-for (let i = 0; i < numberOfElements1; i++){
-    let valueVector1 = parseFloat(prompt("Gib die Werte der Elemente für den ersten Vektor an"));
-    vector1[i] = valueVector1;
-};
-console.log(vector1);
-
-let vector2 = [];
-let numberOfElements2 = parseFloat(prompt("Gib die Menge der Elemente für den zweiten Vektor an"));
-for (let i = 0; i < numberOfElements2; i++){
-    let valueVector2 = parseFloat(prompt("Gib die Werte der Elemente für den zweiten Vektor an"));
-    vector2[i] = valueVector2;
-};
-
-console.log(vector2);
-
-let result = [];
-function vectorAdd(vector1,vector2, result){
-    result[0] = vector1[0] + vector2[0];
-    result[1] = vector1[1] + vector2[1];
-    result[2] = vector1[2] + vector2[2];
-    console.log(result);
+function calculate(){
+    let result = [];
+    let vector1 = inputToVector("vector1");
+    let vector2 = inputToVector("vector2");
+    document.getElementById("result").innerText = "BlaBlaBla";
+    let operator = prompt("Gib an, welcher mathematische Operator (+, -, *, /) genutzt werden soll");
+    if (operator === "+") {
+        for (let i = 0; i < vector1.length; i++){
+            result[i] = vector1[i] + vector2[i];
+        };
+    } else if (operator === "-") {
+        for (let i = 0; i < vector1.length; i++){
+            result[i] = vector1[i] - vector2[i];
+        };
+    } else if (operator === "*"){
+        for (let i = 0; i < vector1.length; i++){
+            result[i] = vector1[i] * vector2[i];
+        };
+    } else if (operator !== "/"){
+    } else  {
+        console.log("Eine Division durch Null ist nicht erlaubt!!!");
+    };
+    alert(result);
 };
 
-vectorAdd(vector1,vector2, result);
+function isVectorInFormat(input){
+    const vectorRegex = /^\[\s*(-?\d+(\.\d+)?\s*,\s*)*-?\d+(\.\d+)?\s*\]$/;
+    return vectorRegex.test(input);
+}
 
-//Subtrahieren:
-let vector1 = [];
-let numberOfElements1 = parseFloat(prompt("Gib die Menge der Elemente für den ersten Vektor an"));
-for (let i = 0; i < numberOfElements1; i++){
-    let valueVector1 = parseFloat(prompt("Gib die Werte der Elemente für den ersten Vektor an"));
-    vector1[i] = valueVector1;
-};
-console.log(vector1);
-
-let vector2 = [];
-let numberOfElements2 = parseFloat(prompt("Gib die Menge der Elemente für den zweiten Vektor an"));
-for (let i = 0; i < numberOfElements2; i++){
-    let valueVector2 = parseFloat(prompt("Gib die Werte der Elemente für den zweiten Vektor an"));
-    vector2[i] = valueVector2;
-};
-
-console.log(vector2);
-
-let result = [];
-function vectorSubtraction(vector1,vector2, result){
-    result[0] = vector1[0] - vector2[0];
-    result[1] = vector1[1] - vector2[1];
-    result[2] = vector1[2] - vector2[2];
-    console.log(result);
-};
-
-vectorSubtraction(vector1,vector2, result);
+function inputToVector(inputId){
+    let variable = document.getElementById(inputId);
+    console.log(variable);
+    let value = variable.value;
+    isVectorInFormat(inputId);
+    return value;
+}
 
 
-//Multiplikation:
 
-let vector1 = [];
-let numberOfElements1 = parseFloat(prompt("Gib die Menge der Elemente für den ersten Vektor an"));
-for (let i = 0; i < numberOfElements1; i++){
-    let valueVector1 = parseFloat(prompt("Gib die Werte der Elemente für den ersten Vektor an"));
-    vector1[i] = valueVector1;
-};
-console.log(vector1);
-
-let vector2 = [];
-let numberOfElements2 = parseFloat(prompt("Gib die Menge der Elemente für den zweiten Vektor an"));
-for (let i = 0; i < numberOfElements2; i++){
-    let valueVector2 = parseFloat(prompt("Gib die Werte der Elemente für den zweiten Vektor an"));
-    vector2[i] = valueVector2;
-};
-
-console.log(vector2);
-
-let result = [];
-function vectorMultiplication(vector1,vector2, result){
-    result[0] = vector1[0] * vector2[0];
-    result[1] = vector1[1] * vector2[1];
-    result[2] = vector1[2] * vector2[2];
-    console.log(result);
-};
-
-vectorMultiplication(vector1,vector2, result);
+function createVector(){
+    let vector = [];
+    let numberOfElements = parseFloat(prompt("Gib die Menge der Elemente für den ersten Vektor an"));
+    for (let i = 0; i < numberOfElements; i++){
+        let valueVector = parseFloat(prompt("Gib die Werte der Elemente für den ersten Vektor an"));
+        vector[i] = valueVector;
+    }
+   return vector; 
+}
 
 
-//Division:
 
-let vector1 = [];
-let numberOfElements1 = parseFloat(prompt("Gib die Menge der Elemente für den ersten Vektor an"));
-for (let i = 0; i < numberOfElements1; i++){
-    let valueVector1 = parseFloat(prompt("Gib die Werte der Elemente für den ersten Vektor an"));
-    vector1[i] = valueVector1;
-};
-console.log(vector1);
 
-let vector2 = [];
-let numberOfElements2 = parseFloat(prompt("Gib die Menge der Elemente für den zweiten Vektor an"));
-for (let i = 0; i < numberOfElements2; i++){
-    let valueVector2 = parseFloat(prompt("Gib die Werte der Elemente für den zweiten Vektor an"));
-    vector2[i] = valueVector2;
-};
 
-console.log(vector2);
 
-let result = [];
-function vectorDivision(vector1,vector2, result){
-    result[0] = vector1[0] / vector2[0];
-    result[1] = vector1[1] / vector2[1];
-    result[2] = vector1[2] / vector2[2];
-    console.log(result);
-};
 
-vectorDivision(vector1,vector2, result);
+
+
+
+
+
+
+
+
+
+
+// //Addieren:
+
+// let vector1 = [];
+// let numberOfElements1 = parseFloat(prompt("Gib die Menge der Elemente für den ersten Vektor an"));
+// for (let i = 0; i < numberOfElements1; i++){
+//     let valueVector1 = parseFloat(prompt("Gib die Werte der Elemente für den ersten Vektor an"));
+//     vector1[i] = valueVector1;
+// };
+// console.log(vector1);
+
+// let vector2 = [];
+// let numberOfElements2 = parseFloat(prompt("Gib die Menge der Elemente für den zweiten Vektor an"));
+// for (let i = 0; i < numberOfElements2; i++){
+//     let valueVector2 = parseFloat(prompt("Gib die Werte der Elemente für den zweiten Vektor an"));
+//     vector2[i] = valueVector2;
+// };
+
+// console.log(vector2);
+
+// let result = [];
+// function vectorAdd(vector1,vector2, result){
+//     result[0] = vector1[0] + vector2[0];
+//     result[1] = vector1[1] + vector2[1];
+//     result[2] = vector1[2] + vector2[2];
+//     console.log(result);
+// };
+
+// vectorAdd(vector1,vector2, result);
+
+// //Subtrahieren:
+// let vector1 = [];
+// let numberOfElements1 = parseFloat(prompt("Gib die Menge der Elemente für den ersten Vektor an"));
+// for (let i = 0; i < numberOfElements1; i++){
+//     let valueVector1 = parseFloat(prompt("Gib die Werte der Elemente für den ersten Vektor an"));
+//     vector1[i] = valueVector1;
+// };
+// console.log(vector1);
+
+// let vector2 = [];
+// let numberOfElements2 = parseFloat(prompt("Gib die Menge der Elemente für den zweiten Vektor an"));
+// for (let i = 0; i < numberOfElements2; i++){
+//     let valueVector2 = parseFloat(prompt("Gib die Werte der Elemente für den zweiten Vektor an"));
+//     vector2[i] = valueVector2;
+// };
+
+// console.log(vector2);
+
+// let result = [];
+// function vectorSubtraction(vector1,vector2, result){
+//     result[0] = vector1[0] - vector2[0];
+//     result[1] = vector1[1] - vector2[1];
+//     result[2] = vector1[2] - vector2[2];
+//     console.log(result);
+// };
+
+// vectorSubtraction(vector1,vector2, result);
+
+
+// //Multiplikation:
+
+// let vector1 = [];
+// let numberOfElements1 = parseFloat(prompt("Gib die Menge der Elemente für den ersten Vektor an"));
+// for (let i = 0; i < numberOfElements1; i++){
+//     let valueVector1 = parseFloat(prompt("Gib die Werte der Elemente für den ersten Vektor an"));
+//     vector1[i] = valueVector1;
+// };
+// console.log(vector1);
+
+// let vector2 = [];
+// let numberOfElements2 = parseFloat(prompt("Gib die Menge der Elemente für den zweiten Vektor an"));
+// for (let i = 0; i < numberOfElements2; i++){
+//     let valueVector2 = parseFloat(prompt("Gib die Werte der Elemente für den zweiten Vektor an"));
+//     vector2[i] = valueVector2;
+// };
+
+// console.log(vector2);
+
+// let result = [];
+// function vectorMultiplication(vector1,vector2, result){
+//     result[0] = vector1[0] * vector2[0];
+//     result[1] = vector1[1] * vector2[1];
+//     result[2] = vector1[2] * vector2[2];
+//     console.log(result);
+// };
+
+// vectorMultiplication(vector1,vector2, result);
+
+
+// //Division:
+
+// let vector1 = [];
+// let numberOfElements1 = parseFloat(prompt("Gib die Menge der Elemente für den ersten Vektor an"));
+// for (let i = 0; i < numberOfElements1; i++){
+//     let valueVector1 = parseFloat(prompt("Gib die Werte der Elemente für den ersten Vektor an"));
+//     vector1[i] = valueVector1;
+// };
+// console.log(vector1);
+
+// let vector2 = [];
+// let numberOfElements2 = parseFloat(prompt("Gib die Menge der Elemente für den zweiten Vektor an"));
+// for (let i = 0; i < numberOfElements2; i++){
+//     let valueVector2 = parseFloat(prompt("Gib die Werte der Elemente für den zweiten Vektor an"));
+//     vector2[i] = valueVector2;
+// };
+
+// console.log(vector2);
+
+// let result = [];
+// function vectorDivision(vector1,vector2, result){
+//     result[0] = vector1[0] / vector2[0];
+//     result[1] = vector1[1] / vector2[1];
+//     result[2] = vector1[2] / vector2[2];
+//     console.log(result);
+// };
+
+// vectorDivision(vector1,vector2, result)
---
+++ Notion/Arrays6.js
@@ -0,0 +1,10 @@
+//parseVector(source)
+
+function parseVcetor(source){
+    let vector = prompt("Gib Werte an für den Vektor");
+
+};
+
+//returns array of strings: String.prototype.split();
+//returns new string containing characters of the calling string from
+//(or between) the specified index (or indices);
